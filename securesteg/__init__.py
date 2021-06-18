053ae2dd3d18cfc0397062fe736959886c13c0cf
Suprad Parashar <suprad.s.parashar@gmail.com>
2021-06-18 16:28:51 +0530

    PyPI Ready

---
+++ securesteg/__init__.py
@@ -0,0 +1,7 @@
+from securesteg.securesteg import get_message
+from securesteg.securesteg import get_length
+from securesteg.securesteg import get_metadata
+from securesteg.securesteg import caesar_cipher
+from securesteg.securesteg import get_bits
+from securesteg.securesteg import get_header
+from securesteg.securesteg import SecureSteganography
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Simple Steganography with Added Security'
+LONG_DESCRIPTION = 'A package that allows to hide data in images with added security such as Passwords, Encryption and MAC Addresses.'
+
+# Setting up
+setup(
+	name="securesteg",
+	version=VERSION,
+	author="Suprad S Parashar",
+	author_email="suprad.s.parashar@gmail.com",
+	description=DESCRIPTION,
+	long_description_content_type="text/markdown",
+	packages=find_packages(),
+	install_requires=['pillow', 'getmac'],
+	keywords=['python', 'image', 'data', 'hiding', 'steganography', 'secure'],
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Unix",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: Microsoft :: Windows",
+	]
+)
